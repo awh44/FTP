95954b462b257d2aea78731d7119c10e530368c1
Austin Herring <awh44@drexel.edu>
2015-11-01 17:12:42 -0500

    Removed commented '#defines' from status_t.h

--- programs/include/status_t.h
+++ programs/include/status_t.h
@@ -1,27 +1,3 @@
-/*typedef uint64_t status_t;
-#define SUCCESS                     0
-#define BAD_COMMAND_LINE            1
-#define FILE_OPEN_ERROR             2
-#define FILE_WRITE_ERROR            3
-#define SOCKET_OPEN_ERROR           4
-#define SOCKET_WRITE_ERROR          5
-#define SOCKET_READ_ERROR           6
-#define CONNECTION_ERROR            7
-#define BIND_ERROR                  8
-#define LISTEN_ERROR                9
-#define ACCEPT_ERROR               10
-#define SOCK_NAME_ERROR            11
-#define HOST_ERROR                 12
-#define MEMORY_ERROR               13
-#define ACCEPTING_ERROR            14
-#define LOG_IN_ERROR               15
-#define SERVICE_AVAILIBILITY_ERROR 16
-#define GET_NAME_ERROR             17
-#define TIME_GET_ERROR             18
-#define TIME_STRING_ERROR          19
-#define NON_FATAL_ERROR            20
-*/
-
 typedef enum
 {
 	SUCCESS = 0,
